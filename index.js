3b03c60dfb85fe7c0c28d46101d8bdee74c6adc6
吴烈 <wulie@ffrj.net>
2020-12-23 16:40:49 +0800

    支持更新State

--- index.js
+++ index.js
@@ -1,48 +1,68 @@
 import React from './lib/areact' 
 import ReactDOM from './lib/areact-dom'
 
-class Job extends React.Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '张三',
+      job: '后端工程师',
+      hobby: '看电影'
+    }
+  }
   render() {
     return (
-      <div className="job">我的工作是前端工程师</div>
+      <div className="wrapper">
+        <h1 className="title">hello <span>{ this.state.name }</span></h1>
+        <p>hobby: { this.state.hobby }</p>
+        <Job job={ this.state.job }></Job>
+        <Hobby hobby={ this.state.hobby }></Hobby>
+      </div>
     )
   }
 }
 
-function Button() {
-  const style = {
-    background: 'salmon'
+class Job extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '小讲堂',
+      courses: ['数学', '语文', '英语'],
+      styleObj: {
+        color: 'red',
+        fontWeight: 'bold'
+      }
+    }
+  }
+  
+  render() {
+    return (
+      <div className="job">
+        <h1>欢迎到<span className="name" style={ this.state.styleObj }>{ this.state.name }</span>来学习</h1>
+        <p>我的工作是{ this.props.job }</p>
+        <button onClick = { this.modifyName.bind(this) }>修改名字</button>
+        <button onClick = { this.setStyle.bind(this) }>样式</button>
+      </div>
+    )
+  }
+  modifyName() {
+    let newName = window.prompt('输入标题','小讲堂')
+    this.setState({name: newName})
   }
 
+  setStyle() {
+    this.setState({
+      styleObj: {
+        color: 'blue'
+      }
+    })
+  }
+}
+
+function Hobby(props) {
   return (
-    <button style = { style }>Button</button>
+    <p>我的兴趣是{ props.hobby }</p>
   )
 }
 
-let num = 0
-let timer = null
-let styleObj = {
-  color: 'red',
-  fontSize: '20px'
-}
-
-onStart() //一开始时执行
-
-function onStart() {
-  console.log('click me')
-  timer = setInterval(() => { //启动时，每秒钟计时一次，做一次渲染
-    num++
-    ReactDOM.render((
-      <div className="wrapper">
-        <Job></Job>
-        <h1 style = { styleObj }>Number: { num }</h1>
-        <Button onClick = { onStart }>start</Button>
-        <button onClick = { onPause }>pause</button>
-      </div>
-    ), document.querySelector('#app'))
-  }, 1000)
-}
-
-function onPause() {
-  clearInterval(timer) //点击停止时，清除定时器
-}
+ReactDOM.render(<App></App>, document.querySelector('#app'))
